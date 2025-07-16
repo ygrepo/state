c6723bbd8c7f99405a75e7382d2141c28c69a321
Abhinav Adduri <abhinadduri@gmail.com>
2025-07-15 19:01:58 -0700

    removed outdated and/or unused configs

--- src/state/_cli/_tx/__init__.py
+++ src/state/_cli/_tx/__init__.py
@@ -10,6 +10,6 @@ __all__ = ["run_tx_train", "run_tx_predict", "run_tx_infer", "add_arguments_tx"]
 def add_arguments_tx(parser: ap.ArgumentParser):
     """"""
     subparsers = parser.add_subparsers(required=True, dest="subcommand")
-    add_arguments_train(subparsers.add_parser("train"))
+    add_arguments_train(subparsers.add_parser("train", add_help=False))
     add_arguments_predict(subparsers.add_parser("predict"))
     add_arguments_infer(subparsers.add_parser("infer"))
